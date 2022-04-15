cb98a25222729cb4d08088d86a48be015309fa24
Abject <lucaswurtz0@gmail.com>
2022-04-15 10:54:11 +0200

    save du 15.04.2022 a 10:54

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -4,6 +4,7 @@ import React from "react";
 const Presentation = () => {
   return (
     <div className="presentation-container">
+      {/* GENERAL INFINI LIGNE */}
       <div className="general">
         <h3>
           Présentation de <span>Infini Ligne</span>
@@ -20,29 +21,92 @@ const Presentation = () => {
           avec Open ClassRooms afin d’avoir un diplôme BAC+2 dans le
           développement WEB il l’effectue en Alternance et il passe donc c’est
           journée a codé des applications WEB. On peu dirent aujourd’hui qu’il
-          est Développeur Junior. Le discord a donc commencer à cette tournée
-          vers le développement web en plus du développement FiveM, fessant
-          également des projets DYI comprenant de l’impression 3D, de
-          l’électronique de la programmation ect il a également commencer à
-          partager ses projets la sur le discord. François la rejoint début 2022
-          pour proposer ses projets autour de la conception 3D étant diplômer et
-          travaillant dans le domaine de la conception 3D de matériel agricole.
-          Aujourd’hui Infini Ligne signifie une association de personnes se
-          regroupant autour du monde virtuel comprenant la modélisation 3D, du
-          développement informatique et également autour du domaine du DYI qui
-          se diversifie, vous pouvez rejoindre le Discord ou vous trouverais de
-          l’aide pour vos projets personnel, apprendre de nouvelle chose et
-          apprendre des choses nouvelles à l’autres. Les personnes étant dans la
-          « Team » Infini Ligne sont aujourd’hui disponibles pour réaliser vos
-          projets, vous pouvez les contacter sur le discord directement en
-          ouvrant un ticket afin de proposer votre projet voir avec eux
-          directement comment ils peuvent être réaliser et a quel coût cela
-          reviendrais. La Team infini Ligne est ouverte au recrutement nous
-          sommes ouverts aux propositions afin de nous rejoindre et de
-          collaborée dans nos futurs projets si cela vous intéresse. Nous
-          partageons également nos projets directement sur ce site WEB dans la
-          section « Nos Projets », vous pouvez également consultée la grille de
-          tarification dans l’onglet « Tarifs ».
+          est Développeur Junior.
+        </p>
+        <button className="btn" id="savoirPlus">
+          En Savoir Plus
+        </button>
+        <p className="savoirPlus" id="pSavoirPlus">
+          Le discord a donc commencer à cette tournée vers le développement web
+          en plus du développement FiveM, fessant également des projets DYI
+          comprenant de l’impression 3D, de l’électronique de la programmation
+          ect il a également commencer à partager ses projets la sur le discord.
+          François la rejoint début 2022 pour proposer ses projets autour de la
+          conception 3D étant diplômer et travaillant dans le domaine de la
+          conception 3D de matériel agricole. Aujourd’hui Infini Ligne signifie
+          une association de personnes se regroupant autour du monde virtuel
+          comprenant la modélisation 3D, du développement informatique et
+          également autour du domaine du DYI qui se diversifie, vous pouvez
+          rejoindre le Discord ou vous trouverais de l’aide pour vos projets
+          personnel, apprendre de nouvelle chose et apprendre des choses
+          nouvelles à l’autres. Les personnes étant dans la « Team » Infini
+          Ligne sont aujourd’hui disponibles pour réaliser vos projets, vous
+          pouvez les contacter sur le discord directement en ouvrant un ticket
+          afin de proposer votre projet voir avec eux directement comment ils
+          peuvent être réaliser et a quel coût cela reviendrais. La Team infini
+          Ligne est ouverte au recrutement nous sommes ouverts aux propositions
+          afin de nous rejoindre et de collaborée dans nos futurs projets si
+          cela vous intéresse. Nous partageons également nos projets directement
+          sur ce site WEB dans la section « Nos Projets », vous pouvez également
+          consultée la grille de tarification dans l’onglet « Tarifs ».
+        </p>
+      </div>
+      <div className="underline2"></div>
+      {/* PRESENTATION LUCAS */}
+      <div className="lucas">
+        <h3>
+          Présentation de <span>Lucas</span>
+        </h3>
+        <div className="underline1"></div>
+        <p>
+          Lucas est un jeune homme de 21 ans bientôt 22 ans qui dans son
+          adolescence étant grand fan des jeux vidéo comme la plus part des gens
+          de son âge, quasiment adulte il a commencé à s’intéresser comment
+          fonctionner les serveur de jeux GTA RP au quel ils jouée depuis déjà
+          plusieurs année après quelque mois de recherche il a configurée sa
+          première base pour y jouer avec ses amis, très vite il a commencer a
+          chercher a comprendre comment fonctionner les script, a les modifier à
+          sa guise puis il a commencer a crée ses propres scripts.
+        </p>
+        <button className="btn" id="savoirPlus">
+          En Savoir Plus
+        </button>
+        <p className="savoirPlus" id="pSavoirPlus">
+          Début 2021 un grand pic de nouveau joueur arrivent sur GTA RP une mode
+          se lance autour de ce style de jeux depuis quelque mois et cela agasse
+          Lucas que les nouveaux joueurs ne respectent pas les règles qui sont
+          en place sur ce type de jeu depuis plusieurs années et décide de
+          fermer son server et de juste crée des scripts gratuit et payant,
+          script unique et crée à la demande de la personne. Après sa il a
+          réalisé que la maintenance industrielle domaine dans le quel il
+          travail depuis l’âge de 15 ans ne l’intéresse pas et il cherche à se
+          reconvertir dans le domaine du développement WEB. Ce qui donne la
+          capacitée à Lucas de crée vos site WEB complet et selon vos besoins,
+          mais également de vous faire des packs de server GTA RP crée
+          spécialement pour vous avec un site web et un server discord pour
+          votre server de jeu. Avec des compétences dans le Hardware Lucas peu
+          également assemblée votre PC vous le mettre en places.
+        </p>
+      </div>
+      <div className="underline2"></div>
+      {/* PRESENTATION FRANCOIS */}
+      <div className="francois">
+        <h3>
+          Présentation de <span>François</span>
+        </h3>
+        <div className="underline1"></div>
+        <p>
+          François est une personne qui a toujours était attirée part le domaine
+          de la modélisation 3D adolescent quand il jouée a farming Simulator
+          avec Lucas et leurs Team FS il étant le modeur / mappeur dédier il
+          savait crée nos map et nos mods afin de se diversifier de ce que les
+          autres jouer, aujourd’hui encore il fait du mapping farming a ses
+          heures perdues mais il a surtout fait de la modélisation 3D sont
+          métier, concepteur 3D de matériel Agricole avec des études jusqu’au
+          BAC+3. Il vous propose aujourd’hui de concevoir vos 3D pour vos
+          différents projets et il vous partage ses projets de modélisation 3D
+          personnel en vous partagent les fichiers afin de les imprimer en 3D ou
+          de les utiliser à votre guise.
         </p>
       </div>
     </div>
